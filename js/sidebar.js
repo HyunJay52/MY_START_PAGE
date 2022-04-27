e5f0bac4b058607ab779dacf46552cc147466261
HyunJay52 <hyunjungk52@gmail.com>
2022-04-28 00:14:17 +0900

    working on shortcut

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,21 @@
 const side = document.querySelector("#sideBtn");
 const list = document.querySelector("#sidebarList");
 const shortCut = list.querySelector("li:last-child");
+const sc_div = shortCut.querySelector("#myShortcut");
+
+let shortcut_url = [];
+const SHORTCUT_KEY = ""
+
+function addShortcutUrl(event) {
+    if(event.keyCode === 13) {
+        const sc_input = sc_div.querySelector("input");
+        shortcut_url.push(sc_input.value);
+        localStorage.setItem(SHORTCUT_KEY, JSON.stringify(shortcut_url));
+        sc_input.value = "";
+    } 
+}
 
 function handleShortcut() {
-    const sc_div = shortCut.querySelector("#myShortcut");
     const sc_toggle = sc_div.classList.contains(HIDDEN_CLASSNAME);
     
     if(!sc_toggle) {
@@ -25,3 +37,4 @@ function handleSidebar() {
 
 side.addEventListener("click", handleSidebar);
 shortCut.addEventListener("click", handleShortcut);
+sc_div.addEventListener("keyup", addShortcutUrl);
